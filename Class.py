fdfed220fd55c7b5548d677887ffc33c43c33c41
lfctatung <37881901+lfctatung@users.noreply.github.com>
2018-04-29 13:04:38 -0700

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,10 @@
+import module1
+Class Number:
+  function __init__(self, a, b):
+    self.a = a; self.b = b
+  function add(self):
+    return self.a + self.b
+
+a = 3; b = 2912   
+twoNums = Number(a, b)
+print("%d + %d = %d" % (a, b, twoNumbs.add()))
